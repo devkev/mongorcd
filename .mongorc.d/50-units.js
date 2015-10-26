ed94559837e4dfb549a5cdfc86d64a37958816fd
Kevin Pulo <kevin.pulo@mongodb.com>
2015-10-26 15:02:25 +1100

    Add 50-units

---
+++ .mongorc.d/50-units.js
@@ -0,0 +1,14 @@
+
+// Useful for passing to db.stats() and similar.
+KiB = 1024;
+MiB = 1024 * KiB;
+GiB = 1024 * MiB;
+TiB = 1024 * GiB;
+PiB = 1024 * TiB;
+
+KB = 1000;
+MB = 1000 * KB;
+GB = 1000 * MB;
+TB = 1000 * GB;
+PB = 1000 * TB;
+
